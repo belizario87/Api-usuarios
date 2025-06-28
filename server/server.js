14a84b43d71e662d8f0d42ec6da4be1e983ce20a
belizario87 <leonardobelizario88@gmail.com>
2025-06-28 10:39:36 -0300

    add configs de servidor

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import usuarios from "../database/database.js";
+import ControllerUsuarios from "../controller/usuarioController.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/usuarios", (req, res) => {
+  res.json(ControllerUsuarios.listarUsuarios());
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
